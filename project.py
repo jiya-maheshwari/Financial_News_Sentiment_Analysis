bf67b2366904ced0d30635ad736b2c562c2a0378
jiya-maheshwari <jiyamaheshwari.10@gmail.com>
2025-01-07 05:05:16 +0530

    data cleaning and visualization update

--- project.py
+++ project.py
@@ -3,8 +3,11 @@ from urllib.request import urlopen,Request
 import pandas as pd
 from datetime import date
 from datetime import timedelta
-# import nltk
-# from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
+import nltk
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
+
+#web scraping 
 
 url = 'https://finviz.com/quote.ashx?t='
 
@@ -38,7 +41,43 @@ for ticker in tickers:
     news_tables[ticker+'_date'] = date_list
     news_tables[ticker+'_time'] = time_list
 
+##sentiment analysis using nltk + preprocessing
+
 titles_df = pd.DataFrame(news_tables)
+func = lambda x: SentimentIntensityAnalyzer().polarity_scores(x)['compound'] if x else 0
+
+for ticker in tickers:
+    titles_df[ticker+'_compound_score'] = titles_df[ticker].apply(func)
+
+print(titles_df)
+
+#visualization 
+nvdia_mean = titles_df[['NVDA_date','NVDA_compound_score']].groupby(['NVDA_date']).mean().reset_index()
+tsla_mean = titles_df[['TSLA_date','TSLA_compound_score']].groupby(['TSLA_date']).mean().reset_index()
+aapl_mean = titles_df[['AAPL_date','AAPL_compound_score']].groupby(['AAPL_date']).mean().reset_index()
+
+mean_df = pd.DataFrame()
+mean_df['Date'] = nvdia_mean['NVDA_date']
+mean_df['NVDA'] = nvdia_mean['NVDA_compound_score']
+mean_df['TSLA'] = tsla_mean['TSLA_compound_score']
+mean_df['AAPL'] = aapl_mean['AAPL_compound_score']
+plt.figure()
+mean_df.plot(kind = 'bar')
+plt.show()
+
+
+
+
+
+#model
+
+
+
+
+
+
+
+
 
 
 
