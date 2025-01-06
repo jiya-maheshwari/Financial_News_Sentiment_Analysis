0ae40d63d294ebd98f063ed710f4d64f9e1a7a27
jiya-maheshwari <jiyamaheshwari.10@gmail.com>
2025-01-07 03:29:42 +0530

    complete web scraping

--- project.py
+++ project.py
@@ -1,5 +1,10 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen,Request
+import pandas as pd
+from datetime import date
+from datetime import timedelta
+# import nltk
+# from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
 
 url = 'https://finviz.com/quote.ashx?t='
 
@@ -8,14 +13,35 @@ tickers = ['NVDA','TSLA','AAPL']
 end = '&ty=c&ta=1&p=d'
 
 news_tables = {}
-
 for ticker in tickers:
+    list_of_titles = []
+    time_list = []
+    date_list = []
     finviz = url+ticker+end
     req = Request(url = finviz, headers={'user-agent':'portfolio-project'})
     response = urlopen(req)
-    html = BeautifulSoup(response,'html')
-    news_table = html.find(id = 'news-table')
-    news_tables[ticker] = news_table
-    break
-print(news_table)
+    html = BeautifulSoup(response,'html.parser')
+    titles = html.find_all('a', class_='tab-link-news')
+    for title in titles:
+        list_of_titles.append(title.text.strip())
+    news_tables[ticker] = list_of_titles
+    timestamps = html.find_all('td', attrs={'align': 'right', 'width': '130'})
+    for timestamp in timestamps:
+        dates = timestamp.text.strip().split(' ')
+        if len(dates) == 1:
+            time = dates[0]
+        else:
+            date = dates[0]
+            time = dates[1]
+        time_list.append(time)
+        date_list.append(date)
+    news_tables[ticker+'_date'] = date_list
+    news_tables[ticker+'_time'] = time_list
+
+titles_df = pd.DataFrame(news_tables)
+
+
+
+
+
 
